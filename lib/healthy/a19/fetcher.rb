6da4a9f1b486f4e54718f025b7827d1859370207
Marten Veldthuis <marten@veldthuis.com>
2013-09-26 12:12:46 +0200

    Refactor

--- lib/healthy/a19/fetcher.rb
+++ lib/healthy/a19/fetcher.rb
@@ -12,12 +12,25 @@ module Healthy
 
       def fetch
         response = mirth_response
-        ensure_valid_response(response)
-        data = parse_response(response.body)
-        data
+        raise_unless_valid_response(response)
+        parse_response(response.body)
       end
 
-      def ensure_valid_response(response)
+      def mirth_response
+        Net::HTTP.start(remote_url.host, remote_url.port, use_ssl: use_ssl?) do |http|
+          request = Net::HTTP::Post.new(remote_url.path)
+          request.set_form_data(mirth_params)
+          http.request request
+        end
+      rescue ::Timeout::Error, Errno::ETIMEDOUT
+        raise ::Healthy::Timeout
+      rescue Errno::EHOSTUNREACH
+        raise ::Healthy::HostUnreachable
+      rescue Errno::ECONNREFUSED
+        raise ::Healthy::ConnectionRefused
+      end
+
+      def raise_unless_valid_response(response)
         return true if response.code == '200'
         raise "Request not successful for patient #{patient_id}. Got: #{response.code}"
       end
@@ -29,25 +42,14 @@ module Healthy
         raise IllegalMirthResponse if e.message =~ /Illegal character '&' in raw string/
       end
 
-      def mirth_response
-        params = {'method' => 'A19',
-                  'patient_id' => patient_id.to_s,
-                  'application' => "healthy"}
-        use_ssl = (remote_url.port == 443 or remote_url.scheme == 'https')
+      private
 
-        Net::HTTP.start(remote_url.host, remote_url.port, use_ssl: use_ssl) do |http|
-          request = Net::HTTP::Post.new(remote_url.path)
-          request.set_form_data(params)
-          http.request request
-        end
-      rescue ::Timeout::Error => e
-        raise ::Healthy::Timeout
-      rescue Errno::ETIMEDOUT => e
-        raise ::Healthy::Timeout
-      rescue Errno::EHOSTUNREACH => e
-        raise ::Healthy::HostUnreachable
-      rescue Errno::ECONNREFUSED => e
-        raise ::Healthy::ConnectionRefused
+      def mirth_params
+        {'method' => 'A19', 'patient_id' => patient_id.to_s, 'application' => "healthy"}
+      end
+
+      def use_ssl?
+        remote_url.port == 443 or remote_url.scheme == 'https'
       end
 
       def remote_url
