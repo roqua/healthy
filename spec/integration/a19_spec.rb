5354d781650250083a7983428c774ee60222c963
Marten Veldthuis <marten@veldthuis.com>
2013-09-26 11:49:56 +0200

    Add Transformer code from mirth, pending integration test

--- spec/integration/a19_spec.rb
+++ spec/integration/a19_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe 'Fetching remote patient details' do
-
+  it 'returns transformed data' do
+    pending
+    load_fixture 'xmcare_patient_with_maiden_name'
+    Healthy::A19.fetch("123").firstname.should == 'Babette'
+  end
 end
