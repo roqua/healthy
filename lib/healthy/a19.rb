8e84244870e91fa7393f9727e6542813d69324d0
Marten Veldthuis <marten@veldthuis.com>
2013-09-30 15:51:49 +0200

    Documentation, but not too much

--- lib/healthy/a19.rb
+++ lib/healthy/a19.rb
@@ -1,5 +1,11 @@
 module Healthy
   module A19
+    # Fetches a patient record given a `patient_id` and returns a hash containing
+    # the interesting information that was returned from an upstream `ADR^A19`
+    # response.
+    #
+    # @param  patient_id [String] the patient identifier
+    # @return [Hash] the patient details.
     def self.fetch(patient_id)
       message = Fetcher.new(patient_id).fetch
       Transformer.new(message).to_patient
--- lib/healthy/a19/cdis_name_parser.rb
+++ lib/healthy/a19/cdis_name_parser.rb
@@ -2,6 +2,8 @@ require 'healthy/a19/name_parser'
 
 module Healthy
   module A19
+    # The CDIS EPD returns names in a format different from most other EPD vendors.
+    # This parser overrides some methods that are affected by the differences.
     class CdisNameParser < NameParser
       def firstname
         names[:legal].fetch('PID.5.2')
