34d7665f11a807a4179ecb488fcb154fd52ef2f5
Arnold van der Meulen <zwippie@gmail.com>
2017-04-11 15:51:04 +0200

    Use http basic auth (#17)

--- lib/roqua/healthy/client.rb
+++ lib/roqua/healthy/client.rb
@@ -4,14 +4,20 @@ module Roqua
     class Client
       include ::Roqua::Support::Instrumentation
 
-      attr_accessor :a19_endpoint
+      attr_accessor :a19_endpoint, :a19_username, :a19_password
 
       def initialize(options = {})
         @a19_endpoint = options[:a19_endpoint]
+        @a19_username = options[:a19_username]
+        @a19_password = options[:a19_password]
       end
 
       def a19_endpoint
         @a19_endpoint || Roqua::Healthy.a19_endpoint
+      end
+
+      def use_basic_auth?
+        @a19_username.present? || @a19_password.present?
       end
 
       def fetch_a19(patient_id)
--- spec/unit/client_spec.rb
+++ spec/unit/client_spec.rb
@@ -3,9 +3,18 @@ require 'spec_helper'
 
 describe Roqua::Healthy::Client do
   context 'fully configured client' do
-    subject { Roqua::Healthy::Client.new(a19_endpoint: 'http://a19_endpoint.dev') }
+    subject do
+      Roqua::Healthy::Client.new(
+        a19_endpoint: 'http://a19_endpoint.dev',
+        a19_username: 'foo',
+        a19_password: 'bar'
+      )
+    end
 
     it { expect(subject.a19_endpoint).to eq 'http://a19_endpoint.dev' }
+    it { expect(subject.a19_username).to eq 'foo' }
+    it { expect(subject.a19_password).to eq 'bar' }
+    it { expect(subject.use_basic_auth?).to be_truthy }
   end
 
   context 'unconfigured client' do
@@ -14,5 +23,7 @@ describe Roqua::Healthy::Client do
     it 'defaults to system wide config' do
       expect(subject.a19_endpoint).to eq Roqua::Healthy.a19_endpoint
     end
+
+    it { expect(subject.use_basic_auth?).to be_falsey }
   end
 end
